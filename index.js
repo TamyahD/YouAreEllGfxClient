7df1c336aa204fb84065b61b5f189250b40c0af1
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-08-07 02:15:38 -0400

    guess who can now see messages in UI :) me

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 import MessageService from "./message-service.js";
 
 let userID = "didntThinkIWouldGetThisFar";
-const messageService = new MessageService();
+const messageService = new MessageService(userID);
 
 window.addEventListener("load", function() {
-    this.document.getElementById("greeting").innerHTML = `Wassup ${userID}!`;
-    messageService.getAllMessages();
+    document.getElementById("greeting").innerHTML = `Wassup ${userID}!`;
+    messageService.getAllMessages()
+        .then(successfulCallback, errorCallback);
+
+    function successfulCallback(response) {
+        // console.log(response);
+        populateThread(response);
+    }
+
+    function errorCallback(response) {
+        console.log(response);
+    }
 });
+
+function populateThread(messages) {
+    messages.forEach(message => {
+        const messageListItem = document.createElement("LI");
+        const userIDHeading = document.createElement("h3");
+        const messageParagraph = document.createElement("p");
+
+        const messageContent = document.createTextNode(message.message);
+        const userIDContent = document.createTextNode(message.fromID);
+
+        userIDHeading.appendChild(userIDContent);
+        messageParagraph.appendChild(messageContent);
+        messageListItem
+            .appendChild(userIDHeading)
+            .appendChild(messageParagraph);
+        document.getElementById("message-list").appendChild(messageListItem);
+    })
+}
